b7d7bc97248d038a000db7b0564d2246be52ec7f
benjaminadk <benjaminadk@gmail.com>
2019-08-21 15:22:56 -0700

    :mag: Improve product seo

--- components/Product/index.js
+++ components/Product/index.js
@@ -10,38 +10,8 @@ import DisplayError from '../DisplayError'
 import { ProductStyles } from './styles/Product'
 
 export default function Product({ query: { id }, user }) {
-  const [seoProduct, setSeoProduct] = useState(null)
   return (
     <>
-      <NextSeo
-        config={{
-          titleTemplate: `Anne's Handmade | %s`,
-          title: seoProduct ? seoProduct.title : 'Product',
-          canonical: `https://anneshandmade.herokuapp.com/product?id=${id}`,
-          facebook: {
-            appId: '2300738536877547'
-          },
-          openGraph: {
-            type: 'website',
-            title: `Anne's Handmade | ${
-              seoProduct ? seoProduct.title : 'Product'
-            }`,
-            url: `https://anneshandmade.herokuapp.com/product?id=${id}`,
-            description:
-              'One of a kind, handmade jewelry @ affordable prices. 💎',
-            site_name: "Anne's Handmade",
-            images: [
-              {
-                url: seoProduct ? seoProduct.images[0] : '',
-                width: 800,
-                height: 800,
-                alt: seoProduct ? seoProduct.title : 'Product'
-              }
-            ]
-          }
-        }}
-      />
-
       <Query query={SINGLE_PRODUCT_QUERY} variables={{ id }}>
         {({ data, loading, error }) => {
           if (loading) {
@@ -51,16 +21,47 @@ export default function Product({ query: { id }, user }) {
             return <DisplayError error={error} />
           }
           const { product, thumbIndex } = data
-          setSeoProduct(product)
-          return (
-            <ProductStyles>
-              <Thumbnails images={product.images} thumbIndex={thumbIndex} />
-              <LargeImage image={product.images[thumbIndex]} />
-              <ProductDetail product={product} user={user} />
-            </ProductStyles>
-          )
+
+          return <ProductPage id={id} user={user} product={product} thumbIndex={thumbIndex} />
         }}
       </Query>
+    </>
+  )
+}
+
+function ProductPage({ id, user, product, thumbIndex }) {
+  return (
+    <>
+      <NextSeo
+        config={{
+          titleTemplate: `Anne's Handmade | %s`,
+          title: product.title,
+          canonical: `https://anneshandmade.herokuapp.com/product?id=${id}`,
+          facebook: {
+            appId: '2300738536877547'
+          },
+          openGraph: {
+            type: 'website',
+            title: `Anne's Handmade | ${product.title}`,
+            url: `https://anneshandmade.herokuapp.com/product?id=${id}`,
+            description: 'One of a kind, handmade jewelry @ affordable prices. 💎',
+            site_name: "Anne's Handmade",
+            images: [
+              {
+                url: product.images[0],
+                width: 800,
+                height: 800,
+                alt: product.title
+              }
+            ]
+          }
+        }}
+      />
+      <ProductStyles>
+        <Thumbnails images={product.images} thumbIndex={thumbIndex} />
+        <LargeImage image={product.images[thumbIndex]} />
+        <ProductDetail product={product} user={user} />
+      </ProductStyles>
     </>
   )
 }
